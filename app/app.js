96529a5cc95923da999471a97c7dab2f17f1f76e
worldtour-demo-crc <castle+worldtour.demo@heroku.com>
2017-03-13 21:51:22 -0700

    Change initial homepage

--- app/app.js
+++ app/app.js
@@ -36,7 +36,7 @@ class MyApp {
             {title: 'Favorites', component: FavoriteListPage, icon: "star"}
         ];
 
-        this.rootPage = WelcomePage;
+        this.rootPage = PropertyListPage;
         this.initializeApp();
     }
 
